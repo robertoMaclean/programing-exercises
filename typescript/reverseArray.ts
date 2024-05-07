ad17ad8beb724afd76119f3fbb03a455779f3889
Roberto Mac lean <roberto.macleanmarinquez@externos-cl.cencosud.com>
2024-05-07 11:46:12 -0400

    chore: format files

--- typescript/reverseArray.ts
+++ typescript/reverseArray.ts
@@ -1,15 +1,15 @@
 const reverseArray = (inputArray: number[], index = 0) => {
-  const midLength = Math.floor(inputArray.length / 2);
+  const midLength = Math.floor(inputArray.length / 2)
   if (midLength > index) {
-    const reverseIndex = inputArray.length - index - 1;
-    const aux = inputArray[reverseIndex];
-    inputArray[reverseIndex] = inputArray[index];
-    inputArray[index] = aux;
-    reverseArray(inputArray, index + 1);
+    const reverseIndex = inputArray.length - index - 1
+    const aux = inputArray[reverseIndex]
+    inputArray[reverseIndex] = inputArray[index]
+    inputArray[index] = aux
+    reverseArray(inputArray, index + 1)
   }
-};
+}
 
-const inputArray = [1, 2, 3, 4, 10];
-console.log(`input: ${inputArray}`);
-reverseArray(inputArray);
-console.log(`output: ${inputArray}`);
+const inputArray = [1, 2, 3, 4, 10]
+console.log(`input: ${inputArray}`)
+reverseArray(inputArray)
+console.log(`output: ${inputArray}`)
